b7f6dca5fbee453e5e2af7a9ffed977d80ec1623
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-05-26 15:15:09 -0500

    wip

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from inventory_scrape import inventory_scrape_wrapper
 if __name__ == "__main__":
     DATE_TIME["date_time"] = "./data/" + date_time_string() + ".db"
     create_tables()
-    delete_oldest_db_file()
     char_names = char_name_scrape()
     asyncio.run(inventory_scrape_wrapper(char_names))
+    delete_oldest_db_file()
